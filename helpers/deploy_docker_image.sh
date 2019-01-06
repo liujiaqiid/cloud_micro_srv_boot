174acb279b0de48068862dd9ae0263ac4e46502e
Jacky.L <liujiaqiid@gmail.com>
2019-01-06 13:55:12 +0800

    Chg jenkins file

--- helpers/deploy_docker_image.sh
+++ helpers/deploy_docker_image.sh
@@ -20,7 +20,7 @@ HELP
   exit 0
 }
 ###################
-while [ -n "$1" ]; do
+while [[ -n "$1" ]]; do
 case "$1" in
    -h) help;shift 1;; # function help is called
    --) shift;break;; # end of options
@@ -33,22 +33,24 @@ done
 
 hostname=$1
 env=$2
-dirname=$3
+projname=$3
 port1=$4
 tag=$5
 uname=$6
 pwd=$7
+
+
 
 # Push local docker image to remote repo
 function step_1_push_image {
     docker login --username=$uname --password=$pwd registry.cn-beijing.aliyuncs.com
     echo "-------------------1.1 after login"
     # Docker tag image
-    docker tag lulucky/$dirname:$tag registry.cn-beijing.aliyuncs.com/lulucky/$dirname:$tag
+    docker tag lulucky/$projname:$tag registry.cn-beijing.aliyuncs.com/lulucky/$projname:$tag
     echo "-------------------1.2 after tag"
     # Docker push image to remote
-    # docker push registry-vpc.cn-beijing.aliyuncs.com/micro-srv/$dirname:$tag
-    docker push registry.cn-beijing.aliyuncs.com/lulucky/$dirname:$tag
+    # docker push registry-vpc.cn-beijing.aliyuncs.com/micro-srv/$projname:$tag
+    docker push registry.cn-beijing.aliyuncs.com/lulucky/$projname:$tag
     echo "-------------------1.3 after push"
     # Remove local images which tag is none
     docker images |grep none |awk '{print $3}'|xargs docker rmi -f
@@ -56,31 +58,42 @@ function step_1_push_image {
 }
 
 # Stop and remove local docker image
-function stop_container {
+function step_2_stop_container {
     # Stop and Remove Container
-    ansible $hostname -u lulucky --sudo -m shell -a "docker ps -a|grep $dirname|awk '{print \$1}'|xargs docker rm -f"
-    echo "after rm container"
+    # ansible $hostname -u lulucky --sudo -m shell -a "docker ps -a|grep $projname|awk '{print \$1}'|xargs docker rm -f"
+    docker ps -a|grep $projname|awk '{print $1}'|xargs docker rm -f
+    echo "-------------------2.1 after rm container"
     # Pull the new image
-    ansible $hostname -u lulucky --sudo -m shell -a "docker pull registry-vpc.cn-beijing.aliyuncs.com/micro-srv:$dirname:$tag"
-    echo "after pull image"
+    # ansible $hostname -u lulucky --sudo -m shell -a "docker pull registry-vpc.cn-beijing.aliyuncs.com/micro-srv:$projname:$tag"
+    docker pull registry.cn-beijing.aliyuncs.com/lulucky/$projname:$tag
+    echo "-------------------2.2 after pull image"
 }
 
 # Launch remote docker image
-function start_container {
+function step_3_start_container {
     # Stop and Remove Docker Image
-    ansible $hostname -u lulucky --sudo -m shell -a "docker images |grep none |awk '{print \$3}'|xargs docker rmi -f"
-    echo "after rm image"
+    # ansible $hostname -u lulucky --sudo -m shell -a "docker images |grep none |awk '{print \$3}'|xargs docker rmi -f"
+    docker images |grep none |awk '{print $3}'|xargs docker rmi -f
+    echo "-------------------3.1 after rm image"
     # Make sure the log directory exists 
-    ansible $hostname -u lulucky --sudo -m file -a "path=/data/logs/$dirname state=directory "
-    echo "after dir"
+    # ansible $hostname -u lulucky --sudo -m file -a "path=/data/logs/$projname state=directory "
+    mkdir -p /data/logs/$projname
+    echo "-------------------3.2 after dir"
     # Run docker image
-    ansible $hostname -u lulucky --sudo -m shell -a "docker run -d -p $port1:$port1  -v /etc/hosts:/etc/hosts  -v /etc/timezone:/etc/timezone -v /etc/localtime:/etc/localtime -v /data/logs/$dirname:/data/logs/$dirname -e "SPRING_PROFILES_ACTIVE=$env"  --name $dirname registry-vpc.cn-beijing.aliyuncs.com/micro-srv/$dirname:$tag "
-    echo "after docker run"
+    # ansible $hostname -u lulucky --sudo -m shell -a "docker run -d -p $port1:$port1  -v /etc/hosts:/etc/hosts  -v /etc/timezone:/etc/timezone -v /etc/localtime:/etc/localtime -v /data/logs/$projname:/data/logs/$projname -e "SPRING_PROFILES_ACTIVE=$env"  --name $projname registry-vpc.cn-beijing.aliyuncs.com/micro-srv/$projname:$tag "
+    docker run -d -p $port1:$port1 \
+        -v /etc/localtime:/etc/localtime \
+        -v /data/logs/$projname:/data/logs/$projname
+        -e "SPRING_PROFILES_ACTIVE=$env" \
+        --restart always \
+        --name $projname registry.cn-beijing.aliyuncs.com/lulucky/$projname:$tag
+        # -t
+    echo "-------------------3.3 after docker run"
 }
 
-echo "before push image"
+echo "-------------------1. before push image"
 step_1_push_image
-echo "before stop container"
-#stop_container
-#echo "before start container"
-#start_container
+echo "-------------------2. before stop container"
+step_2_stop_container
+echo "-------------------3. before start container"
+step_3_start_container
